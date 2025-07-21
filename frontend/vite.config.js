1c9ab09a518ad960c3a7ab75ed76222d6f4d1389
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-07-22 00:01:34 +0530

    again refresh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // ensures correct routing in production
   plugins: [
-     tailwindcss(),
-    react()],
+    tailwindcss(),
+    react()
+  ],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    // This is only for dev; won't affect production,
+    // but good to have for local testing
+    historyApiFallback: true
+  }
 })
