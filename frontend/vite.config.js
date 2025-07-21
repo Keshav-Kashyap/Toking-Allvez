e69da4763a10f505ab2d177e8af2b20346e8fec4
keshav-kashyap <kashyapkeshav934@gmail.com>
2025-07-22 00:07:59 +0530

    netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',  // ensures correct routing in production
+  base: '/', // make sure this is present
   plugins: [
     tailwindcss(),
     react()
   ],
   build: {
     outDir: 'dist'
-  },
-  server: {
-    // This is only for dev; won't affect production,
-    // but good to have for local testing
-    historyApiFallback: true
   }
 })
